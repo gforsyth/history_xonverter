6ccf96d8694605f3a7b7d7f9185ddefbe97fe70b
Gil Forsyth <gilforsyth@gmail.com>
2016-10-28 15:58:53 -0400

    add argparse and require named output file

--- xonsh_hist_to_md.py
+++ xonsh_hist_to_md.py
@@ -1,13 +1,13 @@
 import re
-import sys
 import json
 import jinja2
+import argparse
 
 # regex to remove escape sequences (markdown no like)
 ansi_escape = re.compile(r'(\x9B|\x1B\[)[0-?]*[ -\/]*[@-~]')
 
 # simple input/output template in markdown
-md_in_out = jinja2.Template('''
+md_template = jinja2.Template('''
 ```console
 {{ prompt }} {{ inp|indent(prompt|length + 1)}}
 ```
@@ -18,21 +18,37 @@ md_in_out = jinja2.Template('''
 {% endif %}
 ''')
 
-# load xonsh history
+def get_parser():
+    parser = argparse.ArgumentParser(description="""Naively render a xonsh
+                                    history file into markdown""")
+    parser.add_argument('history_json', type=str,
+                        help='A xonsh history json file')
+    parser.add_argument('output', type=str,
+                        help='Markdown file to write/append to')
+    parser.add_argument('-p', '--prompt', help='Custom prompt character',
+                        default='$')
 
+    return parser
 
-def render_io(history, prompt='$', file='test.md'):
+def render_io(history, outfile, prompt):
     """Loop over history file, printing input and output if output is stored,
     otherwise print nothing
     """
-    with open(file, 'a') as f:
+    with open(outfile, 'a') as f:
         for entry in [i for i in history['data']['cmds'] if 'out' in i]:
             inp = ansi_escape.sub('', entry['inp'])
             out = ansi_escape.sub('', entry['out'])
-            mdout = md_in_out.render(inp=inp, outp=out, prompt=prompt)
+            mdout = md_template.render(inp=inp, outp=out, prompt=prompt)
             f.write(mdout)
 
+def main():
+    parser = get_parser()
+    args = parser.parse_args()
+
+    with open(args.history_json, 'r') as f:
+        history = json.load(f)
+
+    render_io(history, args.output, args.prompt)
+
 if __name__ == '__main__':
-    with open(sys.argv[1], 'r') as f:
-        a = json.load(f)
-    render_io(a)
+    main()
